080fcaf841e6138d66af0c27138c4e1f0112bba5
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-08-27 23:55:42 +0530

    automate the light/dark theme

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,21 +13,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        // Light Theme Colors
-        brightBlue: '#0565F9',        // Primary color: Buttons, links, and accents
-        lightGray: '#EDEEEF',         // Background color: General background, cards
-        mediumGray: '#B2B9C3',        // Secondary text color: Less emphasized text
-        darkGray: '#423F3D',          // Primary text color: Main text
-        nearWhite: '#FEFEFE',         // Surface color: Inputs, cards, and backgrounds that need more emphasis
-
-        // Dark Theme Colors
-        dark: {
-          brightBlue: '#3A7BF1',      // Primary color: Buttons, links, and accents in dark mode
-          lightGray: '#2D2D2D',       // Background color: General background, cards in dark mode
-          mediumGray: '#555555',       // Secondary text color: Less emphasized text in dark mode
-          darkGray: '#1F1F1F',         // Primary text color: Main text in dark mode
-          nearWhite: '#1A1A1A',        // Surface color: Inputs, cards, and emphasized backgrounds in dark mode
-        },
+        
+        primary: 'var(--color-primary)',          // Primary color for buttons, links, etc.
+        background: 'var(--color-background)',    // Background color
+        surface: 'var(--color-surface)',          // Surface elements like cards, inputs, etc.
+        textPrimary: 'var(--color-text-primary)', // Primary text color
+        textSecondary: 'var(--color-text-secondary)', // Secondary text color
       },
     },
   },
