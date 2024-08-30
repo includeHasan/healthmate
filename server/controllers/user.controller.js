6e604f5a566168f429c6049727224cf05fbbda6e
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-08-30 22:01:17 +0530

    backend connection check

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,32 @@
-let welcome= (req, res) => {
-  res.send('Welcome to the User Controller');
+const { PrismaClient } = require('@prisma/client');
+var prisma = new PrismaClient();
+
+let welcome=async(req,res)=>{
+try {
+  let data = await prisma.User.findMany()
+  console.log(data)
+  res.send(data)
+} catch (error) {
+  console.log("yaha error hai"+error)
+  res.status(500).send(error)
+}
+}
+let createUser=async(req,res)=>{
+  let data=req.body
+  console.log(data)
+try {
+    let response=await prisma.User.create({data })
+    res.send(response)
+} catch (error) {
+  console.log("yaha error hai"+error)
+  res.status(500).send(error)
+}
 }
 
-module.exports = {welcome}
+module.exports = {welcome,createUser}
+
+// user_name: 'jayesh_guptrogi',
+// email: 'jayeshChakka122@gmail.com',
+// password: 'jayeshtatti',
+// phone_no: 6969699692,
+// user_type: 'bhikhari'
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
-const { welcome } = require('../controllers/user.controller');
+const { welcome, createUser } = require('../controllers/user.controller');
 var router = express.Router();
 
 router.get('/',welcome)
+router.post('/createUser',createUser)
 module.exports = router;
