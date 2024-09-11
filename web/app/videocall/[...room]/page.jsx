e666267d525e3e4446570e95ed8d2ae4c5ca3239
Jayesh445 <Jayesh040405@gmail.com>
2024-09-11 19:58:03 +0530

    Video call service .env file added

--- web/app/videocall/[...room]/page.jsx
+++ web/app/videocall/[...room]/page.jsx
@@ -5,7 +5,7 @@ import { generateToken04 } from '../server-vcall/zegoServerAssistant';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Room = ({params}) => {
-  
+
     const roomId = params.room[1]; // will set in session, roomid will be the appointment_sechedule_id from the db 
 
     const userId = '';   // userid from the db will be the userid coming from the backend
@@ -15,9 +15,9 @@ const Room = ({params}) => {
     const userName = ""; // it will be the patient or doctor name from the patient and doctor table or can be empty to user to input form the text field to join the call 
     // function to genrate the temperory token from our server and then verify from the zegocloud server 
     const Meeting = (element) => {
-      const appId = 67942341; // genrated from zegocloud
-      const serverSecret = "c22f223a94c74276d2dc4e56898a98ad"; // genrated from zegocloud
-      const token = generateToken04(appId , userId , serverSecret ,effectiveTimeInSeconds , "");
+      // const appId // genrated from zegocloud
+      // const serverSecret // genrated from zegocloud
+      const token = generateToken04(process.env.APP_ID , userId , process.env.SERVER_SECRET ,effectiveTimeInSeconds , "");
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForProduction(appId,token,roomId , userId, userName);
       const zp = ZegoUIKitPrebuilt.create(kitToken);
 
