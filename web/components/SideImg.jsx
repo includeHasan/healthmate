a4711ca2a4a86f54b8f145b1a73145921465b6f2
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-09-27 10:40:22 +0000

    add dynamic image slides with features description

--- web/components/SideImg.jsx
+++ web/components/SideImg.jsx
@@ -1,33 +1,55 @@
 'use client';
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SideImg = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slides = [
+    {
+      src: '/doctor_login.png',
+      title: 'Enhance impact in healthcare',
+      description: 'Your impact in healthcare just got stronger. Enhance patient care through refined data control, seamless appointments, and impactful task management.',
+    },
+    {
+      src: '/another_image.png',
+      title: 'Improve Patient Engagement',
+      description: 'Engage patients with effective communication and streamlined processes.',
+    },
+   
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [slides.length]);
+
   return (
     <div className="w-1/2 bg-blue-600 text-white flex flex-col justify-center items-center p-8">
       <div className="text-3xl font-bold mb-4">HealthHub</div>
       <Image
-        src={'/doctor_login.png'}
-        alt="Doctor Image"
+        src={slides[currentIndex].src}
+        alt="Slide Image"
         className="w-64 h-64 rounded-full object-cover"
-        width={4} height={4}
+        width={256} 
+        height={256}
       />
       <h2 className="text-2xl font-semibold mb-2">
-        Enhance impact in healthcare
+        {slides[currentIndex].title}
       </h2>
       <p className="text-center mb-4">
-        Your impact in healthcare just got stronger. Enhance patient care
-        through refined data control, seamless appointments, and impactful task
-        management.
+        {slides[currentIndex].description}
       </p>
       <div className="flex space-x-2">
-        <span className="w-2 h-2 bg-white rounded-full"></span>
-        <span className="w-2 h-2 bg-white rounded-full opacity-50"></span>
-        <span className="w-2 h-2 bg-white rounded-full opacity-50"></span>
-        <span className="w-2 h-2 bg-white rounded-full opacity-50"></span>
+        {slides.map((_, index) => (
+          <span key={index} className={`w-2 h-2 rounded-full ${index === currentIndex ? 'bg-white' : 'bg-white opacity-50'}`}></span>
+        ))}
       </div>
     </div>
   );
 };
 
 export default SideImg;
+
