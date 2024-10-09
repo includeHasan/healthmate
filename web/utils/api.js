b853b69c965d5d3ff19cba9fd8abdb835211d14e
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-10-10 01:04:24 +0530

    bhai jayesh soch samaj ke ppush kia kar

--- web/utils/api.js
+++ web/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api=axios.create({
-    baseURL:"https://healthmate-backend.vercel.app/",
+    baseURL:"https://healthmate-backend.vercel.app",
     withCredentials:true,
     headers:{
         'Access-Control-Allow-Origin' :true
