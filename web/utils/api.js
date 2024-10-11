7cd62ee53bacb718987dc9cab9e51394ea22dd62
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-10-12 00:22:08 +0530

    sorry

--- web/utils/api.js
+++ web/utils/api.js
@@ -1,9 +1,6 @@
-import axios from "axios"
+import axios from "axios";
 
-export const api=axios.create({
-    baseURL:"https://healthmate-backend.vercel.app",
-    withCredentials:true,
-    headers:{
-        'Access-Control-Allow-Origin' :true
-    }
-})
+export const api = axios.create({
+  baseURL: "https://healthmate-backend.vercel.app",
+  withCredentials: true,  
+});
