a16aadf4781f4c880a71aea8a83d43e12c7fe0af
includeHasan <140363135+includeHasan@users.noreply.github.com>
2024-10-13 01:14:59 +0530

    jayesh deployement wale api please push ke time daldia kr

--- web/utils/api.js
+++ web/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://healthmate-backend.vercel.app",
   withCredentials: true,  
 });
 // "https://healthmate-backend.vercel.app"
