afbbe855d93790986be0b0ab752af724a142c848
Jayesh445 <Jayesh040405@gmail.com>
2024-10-13 15:04:30 +0530

    done kuch to ho

--- server/app.js
+++ server/app.js
@@ -39,19 +39,14 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ['https://healthhmate.vercel.app',`http://localhost:3000`],
+  origin: 'https://healthhmate.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
 // Handle preflight requests
-app.options('*', cors({
-  origin: ['https://healthhmate.vercel.app',`http://localhost:3000`],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.options('*', cors());
 
 
 // Session configuration
