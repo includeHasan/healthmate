da9407ebf76fdac23afabeb4e92252282c7c5575
Jayesh445 <Jayesh040405@gmail.com>
2024-10-13 12:03:28 +0530

    thoda kuch kr

--- web/utils/api.js
+++ web/utils/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://healthmate-backend.vercel.app",
-  withCredentials: true,  
+  withCredentials: true,
 });
 // "https://healthmate-backend.vercel.app"
